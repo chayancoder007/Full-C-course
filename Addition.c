93453184466e37fd92cbcea43fd4b71d6f732b23
The Chayan <91484285+chayancoder007@users.noreply.github.com>
2023-04-18 11:18:51 +0530

    Add files via upload
    simple addition code in "C"

---
+++ Addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void  main()
+
+{
+    int a=10,b=20,c;
+    // here we did inisilization of variable with intiger data type to store values in the variables
+    // in int data type we cnstore interger vlue
+    c=a+b;//condition for addtion 
+    printf("\n Your addtion is : %d",c);   // %d is to give the loccation of c variable in memorry
+
+}
